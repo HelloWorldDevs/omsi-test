7908f29a6c6665ac689e6ab9311d0a3ed8d336b3
Keenan Wells <k.p.wellsy@gmail.com>
2016-11-22 09:54:05 -0800

    added call for posting to page content type

--- js/page/pageData.js
+++ js/page/pageData.js
@@ -93,55 +93,52 @@ var pageDataModule = (function(){
   };
 
   pageData.init = function() {
-
     pageData.dataJsonWrite();
 
-    console.log(window.location.href);
+
+    // $.ajax({
+    //   url:'http://revenant-api.bfdig.com/rest-test',
+    //   method: 'GET'
+    // }).then(function(data) {
+    //   console.log(data);
+    // });
+
+
 
     $.ajax({
-      url:'http://revenant-api.bfdig.com/rest-test',
-      method: 'GET'
-    }).then(function(data) {
-      console.log(data);
-    });
+      type: 'POST',
+      url: 'http://revenant-api.bfdig.com/entity/node',
+      headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/hal+json',
+           'Authorization': 'Basic Og== '
+       },
+       body: JSON.stringify({
+           "_links": {
+             "type": {
+               "href":"http://revenant-api.bfdig.com/rest/type/node/revenant_page"
+             },
+             // "http://revenant-api.bfdig.com/rest/type/node/revenant_page/field_page_node": {
+             //   "href": "http://revenant-api.bfdig.com"
+             // }
 
-
-  //
-  //   $.ajax({
-  //     type: 'POST',
-  //     url: '',
-  //     crossDomain: true,
-  //     cors: true,
-  //     xhrFields: {
-  //       withCredentials: true
-  //     },
-  //     headers: {
-  //          'Accept': 'application/json',
-  //          'Content-Type': 'application/hal+json',
-  //          'Authorization': 'Basic Og== '
-  //      },
-  //      body: JSON.stringify({
-  //          "_links": {
-  //            "type": {
-  //              "href":"http://revenant-api.bfdig.com/rest/type/node/revenant_page"
-  //            }
-  //          },
-  //          "type":[{"target_id":"revenant_page"}],
-  //          "title":[{"value":'Page'}],
-  //          "field_page_url":[{"value":'url'}],
-  //          "field_page_node":[
-  //            {"field_xpath":"xpath"},
-  //            {"field_old_text":"old text"},
-  //            {"field_new_text":"new text"}
-  //          ]
-  //      }),
-  //      success: function() {
-  //        console.log('success')
-  //      },
-  //      error: function (err) {
-  //       console.log("AJAX error in request: " + JSON.stringify(err, null, 2));
-  //   }
-  //   })
+           },
+           "type":[{"target_id":"revenant_page"}],
+           "title":[{"value":'Page'}],
+           "field_page_url":[{"value":'url'}],
+           "field_page_node":[
+             {"field_xpath":"xpath"},
+             {"field_old_text":"old text"},
+             {"field_new_text":"new text"}
+           ]
+       }),
+       success: function() {
+         console.log('success')
+       },
+       error: function (err) {
+        console.log("AJAX error in request: " + JSON.stringify(err, null, 2));
+    }
+    })
   };
 
   return {
